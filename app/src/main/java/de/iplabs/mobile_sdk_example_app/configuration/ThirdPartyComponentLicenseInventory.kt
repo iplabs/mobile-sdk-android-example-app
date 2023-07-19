0fc380a36205f86120e9a4087e0890041ea6dc0d
Matthias Wetter <11140006+m2u-84@users.noreply.github.com>
2023-07-19 16:59:55 +0200

    Add third party component license

--- app/src/main/java/de/iplabs/mobile_sdk_example_app/configuration/ThirdPartyComponentLicenseInventory.kt
+++ app/src/main/java/de/iplabs/mobile_sdk_example_app/configuration/ThirdPartyComponentLicenseInventory.kt
@@ -86,6 +86,14 @@ val thirdPartyComponentLicenseInventory = listOf(
 	),
 	ThirdPartyComponentLicense(
 		licenseName = "Apache License, Version 2.0",
+		licenseFile = R.raw.license_org_jetbrains_kotlin_kotlin_serialization,
+		componentName = "Kotlin Serialization",
+		componentVersion = "1.8.22",
+		componentVendor = "JetBrains s.r.o.",
+		componentLink = "https://github.com/Kotlin/kotlinx.serialization/"
+	),
+	ThirdPartyComponentLicense(
+		licenseName = "Apache License, Version 2.0",
 		licenseFile = R.raw.license_org_jetbrains_kotlin_android,
 		componentName = "Kotlin Standard Library for JVM",
 		componentVersion = "1.8.21",
